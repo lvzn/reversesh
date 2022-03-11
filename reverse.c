1f02a9d76367d84abebe1ce5656505bf67331442
Leevi <joona.mlin@gmail.com>
2022-03-11 18:38:07 +0200

    Added working file reading

--- reverse.c
+++ reverse.c
@@ -1,7 +1,28 @@
+//Sources:
+//1: https://www.tutorialspoint.com/data_structures_algorithms/linked_list_program_in_c.htm
+
 #include <stdio.h>
+#include <stdlib.h>
 
-char* readfile(char* input) {
+// sources used: 1
+struct node {
+    char* line;
+    int key;
+    struct node *next;
+};
 
+char* readFile(char* inputfile) {
+    FILE* f;
+    char *buf;
+    size_t length = 0;
+    if ((f = fopen(inputfile, "r")) == NULL) {
+        puts("File can't be opened.");
+        exit(1);
+    }
+    while (getline(&buf, &length, f) != -1) {
+        printf("%s", buf);    
+    }
+    printf("\n");
     return "";
 }
 
@@ -14,8 +35,9 @@ int main(int argc, char** argv) {
     case 2:
         puts("2 args");
         //read input from file and output to stdout
+        readFile(argv[1]);
         break;
-    case 3: 
+    case 3:
         puts("3 args");
         //read input from file and output to output file
         break;
